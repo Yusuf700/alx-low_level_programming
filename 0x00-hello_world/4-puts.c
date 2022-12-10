22cdd29f43e078331962f72a545e8d4a4cd091a3
Yusuf Ayuba <ayubayusuf117@gmail.com>
2022-12-09 22:15:56 -0800

    sizes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /*
 * main - Entry point
-*
 *Return: Always 0 (Success)
 */
 int main(void)
-{
+// main - Entry point
+{ 
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
