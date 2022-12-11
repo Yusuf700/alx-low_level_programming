55a07075371ab711a8b6c9e29174ff5b086fcb71
Yusuf Ayuba <ayubayusuf117@gmail.com>
2022-12-11 03:54:50 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,10 +5,10 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+	int 1;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	1 = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	return (0);
 }
