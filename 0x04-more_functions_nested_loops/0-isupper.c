db40af4b2ecd90463efdf78edbc14a99dac4ebdc
Yusuf Ayuba <ayubayusuf117@gmail.com>
2022-12-15 03:38:55 -0800

    xamanee boss

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper -function that check if the character isuppercase
+ * @c:input
+ * Return: 1 if  if c is uppercase, 0 therwise.
+ */
+
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+	return (1);
+	else
+	return (0);
+
+	}
+}
