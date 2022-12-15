861fa31c5375afcc83bb6200b920661591a0881d
Yusuf Ayuba <ayubayusuf117@gmail.com>
2022-12-14 22:11:16 -0800

    1-alphabet.c a-z committed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - printsthe alpha in lowercase
+ */
+
+void print_alphabet(void)
+{
+char y;
+
+for (y = 'a'; y <= 'z'; y++)
+_putchar(y);
+
+_putchar('\n');
+
+}
