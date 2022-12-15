01b88fb09b5821d0ddfcfd5224338d1c8c786fb8
Yusuf Ayuba <ayubayusuf117@gmail.com>
2022-12-14 22:36:55 -0800

    2-print_alphabet nested for loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+ #include "main.h"
+
+/**
+ *  print_alphabet_x10 - prints alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+int ten;
+char y;
+for (ten = 0; ten <= 9 ten++)
+{
+for (y = 'a'; y <= 'z' y++)
+_putchar(y);
+_putchar('\n');
+
+}
+return (0);
+}
