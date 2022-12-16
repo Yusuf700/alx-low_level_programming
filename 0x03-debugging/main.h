c42c1f53106b047bcf7957fe823b6548a5c36892
Yusuf Ayuba <ayubayusuf117@gmail.com>
2022-12-15 19:27:28 -0800

    me

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,25 +2,9 @@
 #define MAIN_H
 #include <stdio.h>
 
-<<<<<<< HEAD
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
-=======
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-int _putchar(char c);
-
->>>>>>> b8a338de555b9e36b954f9ea1e8aa5ba110c5549
 
 #endif
