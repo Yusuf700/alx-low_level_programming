c88c51621691f27848125bd9b5d777fb3f0e0d8b
Yusuf Ayuba <ayubayusuf117@gmail.com>
2022-12-19 21:24:33 -0800

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,10 +11,9 @@ void print_rev(char *s)
 
 	while (s[i] != '\0')
 		i++;
-	
-		for (i = i - 1; i >= 0; i--)
-			_putchar(s[i]);
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
 
-		_putchar('\n');
+	_putchar('\n');
 	
 }
