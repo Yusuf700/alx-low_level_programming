908813d2380ea55e2f0991b531ce86f45c1a7b56
Yusuf Ayuba <ayubayusuf117@gmail.com>
2022-12-19 19:55:13 -0800

    swap two integers and points a to b

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - initial swap of two integers
+ * @a: int a
+ * @b: int b
+ */
+
+void swap_int(int *a, int *b);
+{
+	int y;
+
+	a = *y;
+	*a = *b;
+	*b = y;
+}
