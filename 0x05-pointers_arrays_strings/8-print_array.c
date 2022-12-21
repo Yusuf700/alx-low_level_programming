3559b2dc23ea23c921a157e5dbb4af5f0b4859e1
Yusuf Ayuba <ayubayusuf117@gmail.com>
2022-12-20 19:47:17 -0800

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,17 +10,19 @@
 void print_array(int *a, int n)
 {
 int i;
-i = 0;
 
+i = 0;
 while (i < n)
 {
 printf("%d", a[i]);
 
 if (i < n - 1)
 {
-printf(",");
+printf(", ");
 }
+
 i++;
 }
+
 printf("\n");
 }
