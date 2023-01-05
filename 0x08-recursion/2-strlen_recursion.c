68029081285bacfefae27d6e6efb041016134536
Yusuf Ayuba <ayubayusuf117@gmail.com>
2023-01-04 22:36:00 -0800

    print the length of a character

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion -Return the length of a string
+ * @s: is to be measured
+ *
+ * Return: the lenght of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+}
